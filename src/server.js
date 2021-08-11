59a631e95976da687282626def50438b7741a22b
Emmanuel Garnica <egarnica@serempre.com>
2021-08-11 17:20:54 -0500

    first commit

---
+++ src/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    users_api_url: process.env.USERS_API_URL || 'https://usuarios-emmanuel-dw3.herokuapp.com/',
+    account_api_url: process.env.ACCOUNT_API_URL || 'https://cuentas-emmanuel-dw3.herokuapp.com/',
+};
+
